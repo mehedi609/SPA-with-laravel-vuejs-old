c0eaca73c464ac7f7d7193faba2b7494f0ae3d17
mehedi609 <mehedi609@gmail.com>
2019-08-26 23:04:57 +0600

    "initial commit"

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Modal\Category::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+#A Real time Single Page App named Discussion Forum.
+
+##Tools use: `Laravel`, `VueJS`, `Pusher`, `Vuetify`, `markdown`
